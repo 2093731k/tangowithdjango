fb7903cfc0c35785750f97ad97ed0758501a5494
2093731k <vkulazhenkov@gmail.com>
2015-02-23 20:33:53 +0000

    Chapter 12 completed

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from registration.backends.simple.views import RegistrationView
+
+#redirection for users after login to the index page
+class MyRegistrationView(RegistrationView):
+    def get_success_url(selfself,request, user):
+        return '/rango/'
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,7 +15,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls'))
+    url(r'^rango/', include('rango.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='reigstration_register'),
+    (r'^accounts/', include('registration.backends.simple.urls'))
 )
 
 if settings.DEBUG:
